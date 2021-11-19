52af2e758c06025f32cfebd90852546693e3d380
vpr <vpr@mail.ru>
2021-11-19 23:40:45 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+public class Deth8 {
+
+    public Deth8(Needle7 needle) {
+    }
+
+    @Override
+    public String toString() {
+        return "!!!!DEATH!!!!";
+    }
+}
